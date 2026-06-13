a64c552932624a2954ca24625b36b2049c09be22
msiuda <siudamarcin3@gmail.com>
2026-06-13 15:18:50 +0200

    init

---
+++ src/main/kotlin/marcin/siuda/lexilearn/LexilearnApplication.kt
@@ -0,0 +1,11 @@
+package marcin.siuda.lexilearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LexilearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<LexilearnApplication>(*args)
+}
---
+++ src/test/kotlin/marcin/siuda/lexilearn/LexilearnApplicationTests.kt
@@ -0,0 +1,13 @@
+package marcin.siuda.lexilearn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LexilearnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
